61d7c6dcf775d1ba2e00351a93308cfe4193e300
vibhukumar2002 <140388278+vibhukumar2002@users.noreply.github.com>
2023-07-31 11:37:52 +0530

    second change

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import Headerbuttons from './Headerbuttons'
 
 const Header = () => {
 
+  //controlled inputs
+  let [inputsdata,setinputdata]=useState([{searchbar:""}])
+ // console.log(inputsdata)
+
+  let handlechange = (e)=> {
+    
+      setinputdata(prev=>{
+        return(
+          {
+            ...prev,
+            [e.target.name]:e.target.value
+          }
+        )
+      })
+  }
+
+
   return (
     <div id='headercontainer'>
         <h3  id='appname'>instagram</h3>
-        <input type="text"  id='searchbar' name='searchbar' placeholder='Search Instagram'/>
+        <input type="text"  id='searchbar' name='searchbar' placeholder='Search Instagram' onChange={(e)=>handlechange(e)} value={inputsdata.searchbar}/>
         <Headerbuttons/>
         
     </div>
--- src/Signin.js
+++ src/Signin.js
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Signin.css'
 
 const Signin = () => {
+   
+  let [data,setdata]=useState([ 
+    {
+        mail: "",
+        pword: ""
+    }
+  ])
+
+  console.log(data)
+
+  let handlechange=(e)=>
+  {
+       setdata(prev=>
+        {
+          return(
+            {
+              ...prev,
+              [e.target.name]:e.target.value
+            }
+          )
+        })
+  }
+
+
+
   return (
     <div id="signincontainer">
         <h1 id="logo">INSTAGRAM</h1>
 
         <div id="inputs">
-            <input type="text" className="userinput"  placeholder='phone number,Username,Or EMail'/>
-            <input type="password" className="userinput" placeholder='Password' />
+            <input type="text" className="userinput"  placeholder='phone number,Username,Or EMail' name='mail' onChange={(e)=>handlechange(e)} value={data.mail}/>
+            <input type="password" className="userinput" placeholder='Password' name='pword' onChange={(e)=>handlechange(e)} value={data.pword}/>
         </div>
 
         <button id="login">Log In</button>
